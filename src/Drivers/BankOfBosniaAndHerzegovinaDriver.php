a9643f916847fd6bfacd4ac664657a956de6928a
Krzysztof Bielecki <kbielecki@netizens.pl>
2021-09-16 07:48:27 +0200

    add: added support for Bank Of Bosnia And Herzegovina Driver

--- src/Drivers/BankOfBosniaAndHerzegovinaDriver.php
+++ src/Drivers/BankOfBosniaAndHerzegovinaDriver.php
@@ -5,7 +5,9 @@ namespace FlexMindSoftware\CurrencyRate\Drivers;
 use DateTime;
 use FlexMindSoftware\CurrencyRate\Contracts\CurrencyInterface;
 use FlexMindSoftware\CurrencyRate\Models\Currency;
+use FlexMindSoftware\CurrencyRate\Models\CurrencyRate;
 use FlexMindSoftware\CurrencyRate\Models\RateTrait;
+use Illuminate\Support\Facades\Http;
 
 /**
  *
@@ -18,41 +20,66 @@ class BankOfBosniaAndHerzegovinaDriver extends BaseDriver implements CurrencyInt
     /**
      * @const string
      */
-    public const URI = 'https://www.cbbh.ba/CurrencyExchange/GetJsonForPeriod';
+    public const URI = 'https://www.cbbh.ba/CurrencyExchange/GetJson';
     /**
      * @const string
      */
-    public const DRIVER_NAME = 'bank-of-belarus';
+    public const DRIVER_NAME = 'bank-of-bosnia-and-herzegovina';
     /**
      * @var string
      */
     public string $currency = Currency::CUR_BAM;
+    /**
+     * @var array|mixed
+     */
+    private array $jsonData;
 
     /**
      * @param DateTime $date
      *
-     * @return mixed|void
+     * @return void
      */
     public function downloadRates(DateTime $date)
     {
+        $response = Http::asJson()->get(static::URI, $this->getQueryString($date));
+        if ($response->ok()) {
+            $this->jsonData = $response->json();
+            $this->parseResponse();
+            $this->saveInDatabase();
+        }
     }
 
     /**
      * @param DateTime $date
      *
-     * @return string
+     * @return array
      */
-    private function sourceUrl(DateTime $date): string
+    private function getQueryString(DateTime $date): array
     {
-        $queryString = [
-            'dateFrom' => 'Sun,%2028%20Feb%202021%2023:00:00%20GMT',
-            'dateTo' => 'Wed,%2030%20Jun%202021%2022:00:00%20GMT',
+        return [
+            'date' => $date->format("m/d/Y 00:00:00")
         ];
+    }
 
-        return sprintf(
-            '%s?%s',
-            static::URI,
-            http_build_query($queryString)
-        );
+    private function parseResponse()
+    {
+        foreach ($this->jsonData['CurrencyExchangeItems'] ?? [] as $item) {
+            $this->data[] = [
+                'no' => $this->jsonData['Number'],
+                'code' => $item['AlphaCode'],
+                'date' => date('Y-m-d', strtotime($this->jsonData['Date'])),
+                'driver' => static::DRIVER_NAME,
+                'multiplier' => floatval($item['Units']),
+                'rate' => floatval(str_replace(',', '.', $item['Middle']))
+            ];
+        }
+    }
+
+    /**
+     *
+     */
+    protected function saveInDatabase()
+    {
+        CurrencyRate::upsert($this->data, ['no', 'driver', 'code', 'date'], ['rate', 'multiplier']);
     }
 }
