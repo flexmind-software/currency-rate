178845833a2258e9445e7fd8d8c3390cf06b765c
Krzysztof Bielecki <kbielecki@netizens.pl>
2021-09-20 07:19:33 +0200

    WIP: connection to bank

--- src/Drivers/ArmeniaDriver.php
+++ src/Drivers/ArmeniaDriver.php
@@ -3,9 +3,12 @@
 namespace FlexMindSoftware\CurrencyRate\Drivers;
 
 use DateTime;
+use DOMDocument;
+use DOMXPath;
 use FlexMindSoftware\CurrencyRate\Contracts\CurrencyInterface;
 use FlexMindSoftware\CurrencyRate\Models\Currency;
 use FlexMindSoftware\CurrencyRate\Models\RateTrait;
+use Illuminate\Support\Facades\Http;
 
 /**
  *
@@ -18,7 +21,7 @@ class ArmeniaDriver extends BaseDriver implements CurrencyInterface
     /**
      * @const string
      */
-    public const URI = 'https://api.cba.am/ExchangeRatesToCSV.ashx';
+    public const URI = 'https://www.cba.am/en/sitepages/ExchangeArchive.aspx';
     /**
      * @const string
      */
@@ -27,6 +30,10 @@ class ArmeniaDriver extends BaseDriver implements CurrencyInterface
      * @var string
      */
     public string $currency = Currency::CUR_AMD;
+    /**
+     * @var string
+     */
+    private string $html;
 
     /**
      * @param DateTime $date
@@ -35,28 +42,66 @@ class ArmeniaDriver extends BaseDriver implements CurrencyInterface
      */
     public function downloadRates(DateTime $date)
     {
+//        $response = Http::get(static::URI, $this->queryString($date));
+//        if ($response->ok()) {
+//            $this->html = $response->body();
+//            $this->parseResponse();
+//        }
+
+    }
+
+    /**
+     *
+     */
+    private function parseResponse()
+    {
+        $this->data = [];
+
+        libxml_use_internal_errors(true);
+
+        $dom = new DOMDocument('1.0', 'UTF-8');
+        $dom->loadHTML($this->html, LIBXML_NOERROR & LIBXML_NOWARNING );
+        $xpath = new DOMXpath($dom);
+
+        libxml_clear_errors();
+
+        $tableRows = $xpath->query('//*[@id="WebPartWPQ6"]');
+        $div = $tableRows->item(0);
+//        foreach ($tableRows as $row => $tr) {
+//            foreach ($tr->childNodes as $td) {
+//                $this->data[$row][] = $this->clearRow($td->nodeValue);
+//            }
+//            $this->data[$row] = array_values(array_filter($this->data[$row]));
+//        }
+//
+//        $this->data = array_map(function ($item) {
+//            [$multiplier, $code] = explode(' ', $item[1]);
+//
+//            return [
+//                'no' => null,
+//                'code' => $code,
+//                'date' => $this->date->format('Y-m-d'),
+//                'driver' => static::DRIVER_NAME,
+//                'multiplier' => floatval($multiplier),
+//                'rate' => floatval($item[2]),
+//            ];
+//        }, $this->data);
     }
 
     /**
      * @param DateTime $date
      *
-     * @return string
+     * @return array
      */
-    private function sourceUrl(DateTime $date): string
+    private function queryString(DateTime $date): array
     {
-        $queryString = [
+        return [
             'ISOCodes' => 'AED,ARS,AUD,BGN,BRL,BYN,CAD,CHF,CNY,CZK,DKK,EGP,EUR,GBP,GEL,HKD,HUF,ILS,INR,IRR,ISK,' .
                 'JPY,KGS,KRW,KWD,KZT,LBP,LTL,LVL,MDL,MXN,NOK,PLN,RON,RUB,SAR,SEK,SGD,SKK,SYP,TJS,TMT,TRY,UAH,' .
                 'USD,UZS,XDR',
             'DateTo' => $date->format('d/m/Y'),
             'DateFrom' => $date->format('01/01/Y'),
-            'order' => 1,
+//            'order' => 1,
         ];
-
-        return sprintf(
-            '%s?%s',
-            static::URI,
-            http_build_query($queryString)
-        );
     }
 }
