37aae4b98810c8e0d830b4533479400ce0b882ee
Krzysztof Bielecki <qwerkon@gmail.com>
2025-08-20 07:25:01 +0200

    feat: cache rate queries

--- src/Models/RateTrait.php
+++ src/Models/RateTrait.php
@@ -7,6 +7,13 @@ use FlexMindSoftware\CurrencyRate\Enums\CurrencyCode;
 
 trait RateTrait
 {
+    /**
+     * Cache of rates retrieved within a single request.
+     *
+     * @var array<string, array{from: float|null, to: float|null}>
+     */
+    private static array $rateCache = [];
+
     public function rate(CurrencyCode|string $currencyFrom, CurrencyCode|string $currencyTo, DateTime $date)
     {
         $currencyFrom = $currencyFrom instanceof CurrencyCode ? $currencyFrom->value : $currencyFrom;
@@ -30,22 +37,34 @@ trait RateTrait
      * @param CurrencyCode|string $currencyTo
      * @param DateTime $date
      *
-     * @return array
+     * @return array{from: ?float, to: ?float}
      */
     public function retrieveDataToCalculation(CurrencyCode|string $currencyFrom, CurrencyCode|string $currencyTo, DateTime $date): array
     {
         $currencyFrom = $currencyFrom instanceof CurrencyCode ? $currencyFrom->value : $currencyFrom;
         $currencyTo = $currencyTo instanceof CurrencyCode ? $currencyTo->value : $currencyTo;
 
-        $row = CurrencyRate::where('driver', static::DRIVER_NAME)
-            ->whereDate('date', $date->format('Y-m-d'))
-            ->whereIn('code', [$currencyFrom, $currencyTo])
-            ->get()
-            ->pluck('calculate_rate', 'code');
+        $cacheKey = implode('|', [
+            static::DRIVER_NAME,
+            $date->format('Y-m-d'),
+            $currencyFrom,
+            $currencyTo,
+        ]);
 
-        return [
-            'from' => $row->get($currencyFrom),
-            'to' => $row->get($currencyTo),
-        ];
+        if (!array_key_exists($cacheKey, self::$rateCache)) {
+            $row = CurrencyRate::where('driver', static::DRIVER_NAME)
+                ->whereDate('date', $date->format('Y-m-d'))
+                ->whereIn('code', [$currencyFrom, $currencyTo])
+                ->get()
+                ->pluck('calculate_rate', 'code');
+
+            self::$rateCache[$cacheKey] = [
+                'from' => $row->get($currencyFrom),
+                'to' => $row->get($currencyTo),
+            ];
+        }
+
+        return self::$rateCache[$cacheKey];
     }
 }
+
