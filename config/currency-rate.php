ee4e6be4693612b202e302eff4897b1a97eb9da6
Krzysztof Bielecki <qwerkon@gmail.com>
2025-08-27 15:20:18 +0200

    test: cover queue concurrency

--- config/currency-rate.php
+++ config/currency-rate.php
@@ -44,8 +44,10 @@ return [
 
     'cache_store' => env('FLEXMIND_CURRENCY_RATE_CACHE_STORE', 'array'),
 
+    'queue_concurrency' => env('FLEXMIND_CURRENCY_RATE_QUEUE_CONCURRENCY', 10),
+
     'retry' => [
-        'count'  => (int) env('FLEXMIND_CURRENCY_RATE_RETRY_COUNT', 3), 
+        'count'  => (int) env('FLEXMIND_CURRENCY_RATE_RETRY_COUNT', 3),
         'sleep'  => (int) env('FLEXMIND_CURRENCY_RATE_RETRY_SLEEP', 1000),
         'factor' => (int) env('FLEXMIND_CURRENCY_RATE_RETRY_FACTOR', 2),
     ],
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,7 @@ namespace FlexMindSoftware\CurrencyRate\Tests;
 
 use FlexMindSoftware\CurrencyRate\CurrencyRateServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Redis;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
@@ -20,6 +21,36 @@ class TestCase extends Orchestra
         Factory::guessFactoryNamesUsing(
             fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
+
+        $funnel = new class () {
+            public function limit($limit)
+            {
+                return $this;
+            }
+
+            public function block($seconds)
+            {
+                return $this;
+            }
+
+            public function then($success, $failure)
+            {
+                $success();
+            }
+        };
+
+        $redis = new class ($funnel) {
+            public function __construct(private $funnel)
+            {
+            }
+
+            public function funnel($name)
+            {
+                return $this->funnel;
+            }
+        };
+
+        Redis::swap($redis);
     }
 
     protected function getPackageProviders($app)
